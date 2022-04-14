e38077e79ed71f57ec19e6980c7f06ab5bb40a83
Selim Fazylov <selim.fazylov1@gmail.com>
2022-04-14 17:44:26 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid'
+
+class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+
+  render () {
+    <div>
+      <h1>Phonbook</h1>
+      <form>
+        <label>
+          Name
+          <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        </label>
+      </form>
     </div>
-  );
+  };
 };
+
+export default App;
