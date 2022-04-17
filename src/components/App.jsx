428f2836d5b591041ac18c564db7de4f9b447f36
Selim Fazylov <selim.fazylov1@gmail.com>
2022-04-17 03:13:17 +0300

    add finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+// импорт тестового списка контактов
+import defaultData from './data/data';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    // запись тестового списка в локал сторедж
+    // localStorage.setItem('contacts', JSON.stringify(defaultData));
+
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+
+    if (parsedContacts) {
+      return this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('updated!!!!!!');
+
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   formSubmitData = (data) => {
